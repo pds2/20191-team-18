a363aab6594431d855d4d938ea2310c9712d66cc
“gusstavotelles” <gustavinho-telles2011@hotmail.com>
2019-06-05 19:49:27 -0300

    Criação Partida.cpp

---
+++ src/models/Partida.cpp
@@ -0,0 +1,37 @@
+#include "models/Partida.hpp"
+
+using std::string;
+using std::toupper;
+
+Partida::Partida(int rodada, int pontos, int mao) {
+    setRodada(rodada);
+    setPontos(pontos);
+    setMao(mao);
+}
+
+int Partida::getRodada() {
+    return this.rodada;
+}
+
+int Partida::getPontos() {
+    return this.pontos;
+}
+
+int Partida::getMao() {
+    return this.mao;
+}
+
+void Partida::setRodada(int rodada) {
+    this.rodada = rodada;
+}
+
+void Partida::setPontos(int pontos) {
+    this.pontos = pontos;
+}
+
+void Partida::setMao(int mao) {
+    this.mao = mao;
+}
+
+
+
