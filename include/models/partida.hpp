e1fe2740343097b82ce56cb5bfeb45fa25281d4b
Gustavo Telles <36875030+gusstavotelles@users.noreply.github.com>
2019-06-05 18:58:37 -0300

    Add files via upload
    Criação classe Partida

---
+++ include/models/partida.hpp
@@ -0,0 +1,35 @@
+#ifndef PARTIDA_H
+#define PARTIDA_H
+
+#include <iostream>
+#include "exceptions/Exception.hpp"
+
+using std::cout;
+using std::endl;
+using std::string;
+
+class Partida {
+
+  
+ private:
+	
+    int rodada;
+    int pontos; 
+    int mao;
+    
+  public:
+    
+    Partida(int rodada, int pontos, int mao);
+
+    int  getRodada();
+    void setRodada(int rodada);
+    
+    int  getPontos();
+	void setPontos(int pontos);
+
+	int  getMao();
+    void setMao(string mao); 
+
+};
+
+#endif
