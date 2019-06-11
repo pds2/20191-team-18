8c242ad1a94fe3867c6ef8ad33a56b04a364eccf
pepedoni <pedro.moutinho@teknisa.com>
2019-06-11 12:12:15 -0300

    Alteracao de Estutura

---
+++ include/controllers/Bot.hpp
@@ -0,0 +1,48 @@
+#ifndef BOT_H
+#define BOT_H
+
+#include <iostream>
+#include <vector>
+#include "models/Jogador.hpp"
+
+using std::cout;
+using std::endl;
+using std::string;
+
+class Bot : public Jogador {
+
+  public:
+    /**
+     * @brief Construtor da classe Bot
+     * 
+     * @param nome 
+     * @param cartas 
+     */
+    Bot(string nome);
+    /**
+     * @brief Construtor da classe Bot que recebe as cartas em sua mão
+     * 
+     * @param nome 
+     * @param cartas 
+     */
+    Bot(string nome, std::vector<Carta*> cartas);
+
+    /**
+     * @brief Retorna um inteiro correspondete a jogada selecionada pelo Bot
+     * 
+     * @param opcoesAdicionais 
+     * @return int - Jogada selecionada pelo bot
+     */
+    virtual int jogar(std::vector<std::string> opcoesAdicionais) override;
+
+    /**
+     * @brief Permite ao bot escolher se aceita o desafio ou não
+     * 
+     * @param jogadorDesafiante - Jogador que propos o desafio
+     * @param pontos - Quantidade de pontos disputados caso o desafio seja aceito
+     * @return int   - Resposta ao desafio 0 - Correr; 1 - Aceitar; 2 - Aumentar;
+     */
+    virtual int aceitarDesafio(Jogador* jogadorDesafiante, int pontos) override;
+};
+
+#endif
